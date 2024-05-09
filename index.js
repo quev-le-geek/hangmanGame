a4283b7d6d836e1f9f75df5da71adb44fda7af2a
EnolaK <enolak64@gmail.com>
2024-05-09 13:37:15 +0200

    added an other gamemode, a selector of language, a definition tab for english words and improved animations

--- index.js
+++ index.js
@@ -14,17 +14,60 @@ const winTitle = document.getElementById("win")
 const themeSelector = document.getElementById("themeSelector")
 const moon = document.getElementById("moon")
 const sun = document.getElementById("sun")
+const definition = document.getElementById("definition")
+const switcher = document.getElementById("switchGameMode")
+let lang = JSON.parse(localStorage.getItem("lang"))
+const langSelector = document.getElementById("langSelect")
+const applyLang = document.getElementById("applyLang")
+const applyKeyboard = document.getElementById("applyKeyboard")
+const settings = document.getElementById("settings")
+const settingsIco = document.getElementById("settingsIco")
+const dbInfo = document.getElementById("dbInfo")
+const updateDb = document.getElementById("updateDb")
+let hasWin = false
+
+
+if (lang == undefined) {
+    lang = navigator.language
+    if (lang.startsWith("fr")) {
+        lang = "fr"
+    }
+    else if (lang.startsWith("en")) {
+        lang = "en"
+    }
+    else {
+        lang = "en"
+    }
+    console.log(lang);
+    localStorage.setItem("lang", JSON.stringify(lang))
+}
+langSelector.value = lang
+definition.setAttribute("lang", lang)
+
+
+let keyboardLang = localStorage.getItem("keyboardLang")
+console.log(keyboardLang);
+
+if (keyboardLang == undefined || null) {
+    keyboardLang = "qwerty"
+    localStorage.setItem("keyboardLang", keyboardLang)
+}
+body.setAttribute("data-keyboard", keyboardLang)
+const keyboardSelect = document.getElementById("keyboardSelect")
+keyboardSelect.value = keyboardLang
+
 
 let word
 let dbLength = 1000
 let listenerSet = false
 let life = 0
+let countThemeChanged = 0
 
-function changeTheme(){
+function changeTheme() {
     if (darkMode == true) {
         body.setAttribute("data-theme", "light")
     }
-    else{
+    else {
         body.setAttribute("data-theme", "dark")
     }
 }
@@ -36,13 +79,15 @@ if (darkMode == undefined) {
 }
 changeTheme()
 
-const actualTimeStamp = Date.now()
+let actualTimeStamp = Date.now()
 const lastUpdate = JSON.parse(localStorage.getItem("lastUpdate"))
 const timeElapsed = (actualTimeStamp - lastUpdate) / 1000 / 60 / 60
 const updateFreq = 24
-
 console.log("Update", Math.floor(timeElapsed * 100) / 100, "hours ago");
+let UpdateTime = "Last update: " + Math.floor(timeElapsed * 10) / 10 + "h"
+dbInfo.innerHTML = UpdateTime
 if (data == undefined) {
+    debugger
     getData(url, 0)
 }
 
@@ -57,7 +102,7 @@ else if (Math.floor(timeElapsed) >= (updateFreq)) {
 else if (Math.floor(timeElapsed) <= (updateFreq)) {
     data = JSON.parse(data)
     console.log("Already saved");
-    init()
+    init(false)
 }
 
 //get a file
@@ -65,13 +110,19 @@ else if (Math.floor(timeElapsed) <= (updateFreq)) {
 async function getData(urlSet, mode) {
     localStorage.setItem("lastUpdate", JSON.stringify(actualTimeStamp))
     console.log("Has just been saved");
+    console.log(urlSet);
     const result = await fetch(urlSet)
     data = await result.json();
+    console.log(data);
 
-    if (mode == 0) { //mode 0: save the response in localStorage
+    if (mode == 0) { //mode 0: save the response in localStorage and init game
+        saveToLs(data) //ls = localstorage
+        init(false)
+    }
+
+    if (mode == 1) { //mode 1 : save response in localStorage 
         saveToLs(data) //ls = localstorage
     }
-    init()
 }
 
 function saveToLs(data) {
@@ -80,13 +131,58 @@ function saveToLs(data) {
 
 function chooseWord() { //return a random word from the list
     var random
-    // console.log(random >= data.data.length == false);
-    while (random <= data.data.length == false) {
+    while (random <= data[lang].length == false) {
         random = Math.round(Math.random() * dbLength)
     }
     // console.log(random);
-    const word = data.data[random]
+    const word = data[lang][random]
     return word
+}
+
+function createDefinition(def) {
+    const definitionsContainer = document.createElement("div")
+    for (let i = 0; i < def[0].meanings.length; i++) {
+        const meaningBlock = document.createElement("div")
+        meaningBlock.setAttribute("class", "meanings")
+        const meaningNumber = document.createElement("p")
+        meaningNumber.setAttribute("class", "meaningNumber")
+        const meaningNumberText = document.createTextNode(" Meaning " + (i + 1) + ": " + def[0].meanings[0].partOfSpeech)
+        meaningNumber.appendChild(meaningNumberText)
+        meaningBlock.appendChild(meaningNumber)
+        for (let j = 0; j < def[0].meanings[i].definitions.length; j++) {
+            const meaningPara = document.createElement("p")
+            if (j % 2 == 0) {
+                meaningPara.setAttribute("class", "even defPara")
+            }
+            else if (j % 2 == 1) {
+                meaningPara.setAttribute("class", "odd defPara")
+            }
+            const text = j + 1 + ": " + def[0].meanings[i].definitions[j].definition
+            const meaningText = document.createTextNode(text)
+            meaningPara.appendChild(meaningText)
+            meaningBlock.appendChild(meaningPara)
+            definitionsContainer.appendChild(meaningBlock)
+            console.log(definitionsContainer);
+
+        }
+    }
+    console.log(definitionsContainer);
+    definitionsContainer.setAttribute("id", "definitionsContainer")
+    const parentDiv = document.getElementById("meanings")
+    parentDiv.appendChild(definitionsContainer)
+}
+
+function fetchDefinition(word) {
+    console.log(lang);
+    if (lang == "en") {
+        console.log(word);
+        const urlDef = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word
+        console.log(urlDef);
+        fetch(urlDef)
+            .then((response) => response.json())
+            .then((json) => createDefinition(json))
+
+    }
 }
 
 function write(wordToWrite) {
@@ -114,6 +210,105 @@ function setListerners() {
         })
     })
 
+    const link = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab"
+
+    themeSelector.addEventListener("click", () => {
+        countThemeChanged++
+        if (countThemeChanged >= 15) {
+            window.location.href = link
+        }
+        darkMode = !darkMode
+        localStorage.setItem("darkMode", JSON.stringify(darkMode))
+        changeTheme()
+    })
+
+    themeSelector.addEventListener("mouseleave", () => {
+        countThemeChanged = 0
+    })
+
+    restart.addEventListener('click', () => {
+        const gameMode = body.getAttribute("data-gameMode")
+        const word = JSON.parse(localStorage.getItem("word"))
+        if (gameMode == "classic") {
+            restartGame()
+            reset(word)
+            init(false)
+        }
+
+        if (gameMode == "v2") {
+            init(true)
+        }
+    })
+
+    definition.addEventListener("click", () => {
+        const dataSpace = definition.getAttribute("data-Space")
+        // console.log(dataSpace);
+        if (dataSpace == "collapsed") {
+            body.setAttribute("data-space", "deployed")
+            definition.setAttribute("data-space", "deployed")
+            definition.setAttribute("data-alreadyDeployed", "true")
+        }
+        else if (dataSpace == "deployed") {
+            definition.setAttribute("data-space", "collapsed")
+            body.setAttribute("data-space", "collapsed")
+        }
+    })
+
+    switcher.addEventListener("click", () => {
+        const gameMode = body.getAttribute("data-gameMode")
+        const word = JSON.parse(localStorage.getItem("word"))
+
+        if (gameMode == "v2") {
+            restartGame()
+            reset(word)
+            body.setAttribute("data-gameMode", "classic")
+            init(false)
+        }
+        else if (gameMode == "classic") {
+            init(true)
+        }
+    })
+
+    applyLang.addEventListener("click", () => {
+        const word = JSON.parse(localStorage.getItem("word"))
+        const selectedLang = langSelector.value
+        lang = selectedLang
+        localStorage.setItem("lang", JSON.stringify(lang))
+        definition.setAttribute("lang", lang)
+        console.log(lang);
+        restartGame()
+        reset(word)
+        init(false)
+    })
+
+    settingsIco.addEventListener("click", () => {
+        const space = settings.getAttribute("data-space")
+        if (space == "collapsed") {
+            settings.setAttribute("data-space", "deployed")
+        }
+        else {
+            settings.setAttribute("data-space", "collapsed")
+            settings.setAttribute("data-alreadyDeployed", "true")
+        }
+    })
+
+    applyKeyboard.addEventListener("click", () => {
+        keyboardLang = document.getElementById("keyboardSelect").value
+        console.log(keyboardLang);
+        body.setAttribute("data-keyboard", keyboardLang)
+        body.setAttribute("data-keyboard", keyboardLang)
+        localStorage.setItem("keyboardLang", keyboardLang)
+    })
+
+    updateDb.addEventListener("click", () => {
+        getData(url, 1)
+        const lastUpdate = localStorage.getItem("lastUpdate")
+        actualTimeStamp = Date.now()
+        const timeElapsed = (actualTimeStamp - lastUpdate) / 1000 / 60 / 60
+        const UpdateTime = "Last update: " + Math.floor(timeElapsed * 10) / 10 + "h"
+        dbInfo.innerHTML = UpdateTime
+    })
+
     listenerSet = true
 }
 
@@ -123,17 +318,63 @@ function removeListeners(keyName, index) {
     localStorage.setItem("usedLetters", JSON.stringify(usedLetters))
 }
 
-function init() {
-    for (const element of key) {
-        element.setAttribute("data-state","playble")
+function restartGame() {
+    for (const element of hangman) {
+        element.style.display = "none"
     }
+    life = 0
+    body.setAttribute("data-end", "none")
+    const definitionContainer = document.getElementById("definitionsContainer")
+    if (lang == "en" && definitionContainer != null) {
+        console.log(definitionContainer);
+        definitionContainer.remove()
+    }
+    definition.setAttribute("data-gameStatue", "unFinished")
+    const guess = document.getElementById("guess")
+    guess.setAttribute("data-justLoaded", "false")
+    for (const element of key) {
+        element.setAttribute("data-state", "playble")
+    }
+
+}
+
+function init(isV2) {
+    // for (const element of key) {
+    //     element.setAttribute("data-state", "playble")
+    // }
+
+    let word
     if (listenerSet == false) {
         setListerners()
     }
     // console.log(data.data.length);
     var foundLetters = []
-    const word = chooseWord().split("")
+    if (isV2 == false) {
+        word = chooseWord().split("")
+        fetchDefinition(word.join(""))
+    }
+    else if (isV2 == true) {
+        word = prompt("Choose a word")
+        if (word == "" || word == null) {
+            alert("Please choose a valid word")
+            console.log("Please choose a valid word")
+            body.setAttribute("data-gameMode", "classic")
+            return
+        }
+
+        else {
+            const lastWord = JSON.parse(localStorage.getItem("word"))
+            console.log(lastWord);
+            reset(lastWord)
+            body.setAttribute("data-gameMode", "v2")
+            word = word.toLocaleLowerCase()
+            word = word.split("")
+            console.log(word);
+        }
+    }
+
     localStorage.setItem("word", JSON.stringify(word))
+    console.log(word);
     for (let i = 0; i < word.length; i++) {
         foundLetters.push(" ")
     }
@@ -141,15 +382,10 @@ function init() {
     localStorage.setItem("foundLetters", JSON.stringify(foundLetters))
     localStorage.setItem("numberOfFoundLetters", JSON.stringify(0))
     localStorage.setItem("usedLetters", JSON.stringify(["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]))
-    console.log(word)
+    console.log(word.join(""))
 
 }
-function looseGame(word) {
-    reset(word)
-    write(word)
-    looseTile.style.display = "flex"
 
-}
 
 function looseLife() {
     hangman[life].style.display = "block"
@@ -157,19 +393,15 @@ function looseLife() {
 
 }
 
-function winGame() {
-    winTitle.style.display = "flex"
-}
-
 function keyPress(keyName, number) {
-    key[number].setAttribute("data-state","used")
-    var usedLetters = JSON.parse(localStorage.getItem("usedLetters"))
-    var found = false
-    var foundLetters = JSON.parse(localStorage.getItem("foundLetters"))
-    var numberOfFoundLetters = JSON.parse(localStorage.getItem("numberOfFoundLetters"))
-    const word = JSON.parse(localStorage.getItem("word"))
-    if (usedLetters[number] == "") {
-        if (life <= 9) {
+    if (life <= 9 && hasWin == false) {
+        key[number].setAttribute("data-state", "used")
+        var usedLetters = JSON.parse(localStorage.getItem("usedLetters"))
+        var found = false
+        var foundLetters = JSON.parse(localStorage.getItem("foundLetters"))
+        var numberOfFoundLetters = JSON.parse(localStorage.getItem("numberOfFoundLetters"))
+        const word = JSON.parse(localStorage.getItem("word"))
+        if (usedLetters[number] == "") {
             for (let i = 0; i < word.length; i++) {
                 const letter = word[i];
                 if (keyName == letter) {
@@ -185,11 +417,16 @@ function keyPress(keyName, number) {
 
             // console.log("numberOfFoundLetters",numberOfFoundLetters)
             if (numberOfFoundLetters == word.length) {
-                winGame()
+                body.setAttribute("data-end", "win")
+                definition.setAttribute("data-gameStatue", "finished")
+                hasWin = !hasWin
             }
         }
         if (life == 10) {
-            looseGame(word)
+            reset(word)
+            write(word)
+            body.setAttribute("data-end", "lost")
+            definition.setAttribute("data-gameStatue", "finished")
         }
         else {
             reset(foundLetters)
@@ -211,21 +448,5 @@ function reset(word) {
     }
 }
 
-restart.addEventListener('click', () => {
-    const word = JSON.parse(localStorage.getItem("word"))
-    for (const element of hangman) {
-        element.style.display = "none"
-    }
-    reset(word)
-    winTitle.style.display = "none"
-    looseTile.style.display = "none"
-    life = 0
-    init()
-})
 
 
-themeSelector.addEventListener('click', () => {
-    darkMode = !darkMode
-    localStorage.setItem("darkMode", JSON.stringify(darkMode))
-    changeTheme()
-})
